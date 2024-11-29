f9ff03e3a749a5198c0b3346f093118d5287f725
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-11-29 16:02:17 +0100

    add multiple cards

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,28 @@
 import Card from "../card/Card";
+import { posts } from "../../data/posts";
 
 export default function Main() {
+  const publishedPosts = posts.filter((post) => post.published === true);
+  console.log(publishedPosts);
+
   return (
     <main>
       <div className="container">
         <div className="card-row">
-          <Card />
+          {publishedPosts.length ? (
+            publishedPosts.map((post) => {
+              return (
+                <Card
+                  key={post.id}
+                  title={post.title}
+                  img={post.image}
+                  description={post.content}
+                />
+              );
+            })
+          ) : (
+            <div>No post</div>
+          )}
         </div>
       </div>
     </main>
